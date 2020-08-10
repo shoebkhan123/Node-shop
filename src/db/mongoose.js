173204dafdb529f280fe420e6b0ebf11992060cf
Shoeb Khan <34496751+shoebkhan123@users.noreply.github.com>
2020-08-10 21:10:48 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,9 +4,17 @@ const mongoose = require('mongoose');
 const USER = `user-name-her!`;
 const PASS = `password-here!`;
 
-/* connecting to mongoDB server */
+/* connecting to mlab */
 mongoose.connect(`mlab-db-url-with-user-name-and-password`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
+
+
+/* Connect to mongodb locally */
+// mongoose.connect(`mongodb://127.0.0.1:27017/shop`, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// });
